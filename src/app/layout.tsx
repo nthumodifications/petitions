c8c835413f5abe5d567b653dee9fdeb8794e89de
ImJustChew <chewtzihwee@gmail.com>
2023-12-05 03:24:31 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
+import { MainNav } from '@/components/MainNav'
+import { Search } from '@/components/Search'
+import { UserNav } from '@/components/user-nav'
 
-const inter = Inter({ subsets: ['latin'] })
+const fontSans  = FontSans({ subsets: ['latin'], variable: "--font-sans", })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        
+        <div className="hidden flex-col md:flex">
+          <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+              <MainNav className="mx-6" />
+              <div className="ml-auto flex items-center space-x-4">
+                <Search />
+                <UserNav />
+              </div>
+            </div>
+          </div>
+          {children}
+          </div>
+      </body>
     </html>
   )
 }
